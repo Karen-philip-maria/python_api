7680a057128cbd489e03fe307cdc855ad2690c13
karen-maria-philip <karenmariaphilip@gmail.com>
2024-07-17 17:12:13 +0300

    add work

--- api/urls.py
+++ api/urls.py
@@ -1,36 +1,19 @@
-from  django.urls import path
+from django.urls import path
 from .views import StudentListView
-
-from  django.urls import path
-from .views import TeacherListView
-
-from  django.urls import path
-from .views import ClassListView
-
-from  django.urls import path
 from .views import CourseListView
-
-from  django.urls import path
+from .views import ClassListView
+from .views import TeacherListView
 from .views import ClassPeriodListView
 
 
-
 urlpatterns=[
-    path("students/",StudentListView.as_view(), name="student_list_view")
-]
+    path("classperiods/",ClassPeriodListView.as_view(),name="classperiods_list_view"),
 
-urlpatterns=[
-    path("teacher/",TeacherListView.as_view(), name="teacher_list_view")
-]
+    path("teacher/",TeacherListView.as_view(),name="teacher_list_view"),
 
-urlpatterns=[
-    path("class/",ClassListView.as_view(), name="class_list_view")
-]
+    path("students/",StudentListView.as_view(),name="student_list_view"),
 
-urlpatterns=[
-    path("course/",CourseListView.as_view(), name="course_list_view")
-]
+    path("classes/",ClassListView.as_view(),name="classes_list_view"),
 
-urlpatterns=[
-    path("classperiod/",ClassPeriodListView.as_view(), name="classperiod_list_view")
+    path("course/",CourseListView.as_view(),name="course_list_view")
 ]
